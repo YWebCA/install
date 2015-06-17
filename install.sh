b29b762ec4e092bcd26fccf85cc6348040947569
Nathan Stier <nathan.stier@gmail.com>
2015-06-17 11:52:13 -0500

    reduce to admin install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 sudo dscl . create /Users/ywca
 sudo dscl . create /Users/ywca RealName "YWCA"
 sudo dscl . create /Users/ywca hint "Tiny floting whale!"
@@ -9,12 +9,3 @@ sudo dscl . create /Users/ywca UserShell /bin/bash
 sudo dscl . create /Users/ywca NFSHomeDirectory /Users/ywca
 sudo cp -R /System/Library/User\ Template/English.lproj /Users/ywca
 chown -R ywca:staff /Users/ywca
-echo "Installing XCode tools. Please click the install button when prompted"
-xcode-select --install
-read -p "Press [return] once XCode tools are installed" answer
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install caskroom/cask/brew-cask postgresql wget node --without-npm
-brew cask install google-chrome google-drive anvil slack sublime-text alfred dash cheatsheet
-https://www.npmjs.com/install.sh | sh
-ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
